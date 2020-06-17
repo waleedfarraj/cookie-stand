7b7bcc02a97a427e8b58e920e2e80d3940f24ef4
waleedfarraj <waleed.farraj95@gmail.com>
2020-06-17 17:48:55 +0300

    working and dry

--- js/app.js
+++ js/app.js
@@ -8,15 +8,16 @@ var parentElement = document.getElementById('sales');
 var article = document.createElement('article');
 parentElement.appendChild(article);
 var table = document.createElement('table');
-// var branchName = [Seattle,Tokyo,Dubai,Paris,Lima]
+var branchName = [] // to push all the objects we creat to it 
 function Branch(location, minmum, maximum, avagrgeSold) {
-     this.location = location
+    this.location = location
     this.minCust = minmum;
     this.maxCust = maximum;
     this.avgSale = avagrgeSold;
     this.custNum = [];
     this.coockiesPerHour = [];
     this.totalCookies = 0;
+    branchName.push(this);
 
 }
 Branch.prototype.getRandomCustomers = function () {
@@ -25,111 +26,167 @@ Branch.prototype.getRandomCustomers = function () {
         this.custNum.push(newNum)
     }
 };
-    Branch.prototype.coockiesSoldByHour = function () {
-        for (var i = 0; i < workingHours.length; i++) {
-            var numberOfCookies = Math.floor(this.avgSale * this.custNum[i]);
-            this.coockiesPerHour.push(numberOfCookies);
-            this.totalCookies = this.totalCookies + numberOfCookies;
+Branch.prototype.coockiesSoldByHour = function () {
+    for (var i = 0; i < workingHours.length; i++) {
+        var numberOfCookies = Math.floor(this.avgSale * this.custNum[i]);
+        this.coockiesPerHour.push(numberOfCookies);
+        this.totalCookies = this.totalCookies + numberOfCookies;
 
-        };
+    };
+}
+
+Branch.prototype.render = function () {
+
+    var tr = document.createElement('tr');
+    var td = document.createElement('td')
+    td.textContent = this.location
+    tr.appendChild(td);
+    for (var i = 0; i < workingHours.length; i++) {
+        var td = document.createElement('td');
+        td.textContent = this.coockiesPerHour[i]
+        tr.appendChild(td);
     }
-   
-        Branch.prototype.render = function(){
-           
-            var tr = document.createElement('tr');
-            var td = document.createElement('td')
-            td.textContent = this.location
-            tr.appendChild(td);
-            for(var i = 0 ;i < workingHours.length; i++){
-                var td =document.createElement('td');
-                td.textContent =this.coockiesPerHour[i]
-                tr.appendChild(td);}
-                var td = document.createElement('td')
-            td.textContent = this.totalCookies
-            tr.appendChild(td);
+    var td = document.createElement('td')
+    td.textContent = this.totalCookies
+    tr.appendChild(td);
 
-            table.appendChild(tr);
-            article.appendChild(table);
-           
+    table.appendChild(tr);
+    article.appendChild(table);
 
 
 
-        }
 
-        function headerMaker() {
-          
-            var tr = document.createElement('tr');
-            var th = document.createElement('th')
-            th.textContent ="___________"
-            tr.appendChild(th);
-            for(var i = 0 ;i < workingHours.length; i++){
-            var th =document.createElement('th');
-            th.textContent =workingHours[i]
-            tr.appendChild(th);}
-            var th = document.createElement('th')
-            th.textContent ="Daily location total"
-            tr.appendChild(th);
+}
 
-            table.appendChild(tr);
-            article.appendChild(table);
+function headerMaker() {
 
-
-
-        }
-        function footerMaker(){var parentElement = document.getElementById('sales');
-       
-        var tr = document.createElement('tr');
-        var th = document.createElement('th')
-        th.textContent ="totals"
+    var tr = document.createElement('tr');
+    var th = document.createElement('th')
+    th.textContent = "___________"
+    tr.appendChild(th);
+    for (var i = 0; i < workingHours.length; i++) {
+        var th = document.createElement('th');
+        th.textContent = workingHours[i]
         tr.appendChild(th);
-        for(var i = 0 ;i < workingHours.length; i++){
-        var th =document.createElement('th');
-        footerTotal=footerTotal+Seattle.coockiesPerHour[i]+Tokyo.coockiesPerHour[i]+Dubai.coockiesPerHour[i]+Paris.coockiesPerHour[i]+Lima.coockiesPerHour[i]
-        th.textContent =Seattle.coockiesPerHour[i]+Tokyo.coockiesPerHour[i]+Dubai.coockiesPerHour[i]+Paris.coockiesPerHour[i]+Lima.coockiesPerHour[i]
-        tr.appendChild(th);}
-        var th = document.createElement('th')
-        th.textContent =footerTotal;
-        tr.appendChild(th);
-        table.appendChild(tr);
-        article.appendChild(table);
+    }
+    var th = document.createElement('th')
+    th.textContent = "Daily location total"
+    tr.appendChild(th);
 
-            
+    table.appendChild(tr);
+    article.appendChild(table);
+
+
+
+}
+var tr1;
+function footerMaker() {
+    tr1 = tr;
+    var parentElement = document.getElementById('sales');
+
+
+
+    var tr = document.createElement('tr');
+    var th = document.createElement('th');
+    tr.appendChild(th);
+    th.textContent = "Totals"
+
+
+    for (var i = 0; i < workingHours.length; i++) {
+        var th = document.createElement('th');
+        var hourTotal = 0;
+        for (var j = 0; j < branchName.length; j++) {
+            hourTotal += branchName[j].coockiesPerHour[i];
         }
+        footerTotal += hourTotal;
+        th.textContent = hourTotal;
+        tr.appendChild(th);
+    }
 
-        var Seattle = new Branch("Seattle",23, 65, 6.3);
-        Seattle.getRandomCustomers();
-        Seattle.coockiesSoldByHour();
-        headerMaker();
-        Seattle.render();
-        
-        var Tokyo = new Branch("Tokyo",3, 24, 1.2,);
-        Tokyo.getRandomCustomers();
-        Tokyo.coockiesSoldByHour();
-        Tokyo.render();
-        var Dubai = new Branch("Dubai",11,38,3.7)
-        Dubai.getRandomCustomers();
-        Dubai.coockiesSoldByHour();
-        Dubai.render();
-        var Paris = new Branch("Paris",20,38,2.3)
-        Paris.getRandomCustomers();
-        Paris.coockiesSoldByHour();
-        Paris.render();
-        var Lima = new Branch("lima",2,16,4.6)
-        Lima.getRandomCustomers();
-        Lima.coockiesSoldByHour();
-        Lima.render();
-        footerMaker();
-        console.log(Seattle);
+    var th = document.createElement('th')
+    th.textContent = footerTotal;
+    tr.appendChild(th);
+    table.appendChild(tr);
+    article.appendChild(table);
 
 
+}
+
+headerMaker();
+var Seattle = new Branch("Seattle", 23, 65, 6.3);
+var Tokyo = new Branch("Tokyo", 3, 24, 1.2,);
+var Dubai = new Branch("Dubai", 11, 38, 3.7);
+var Paris = new Branch("Paris", 20, 38, 2.3);
+var Lima = new Branch("lima", 2, 16, 4.6);
+for( var i =0; i <branchName.length;i++){
+     branchName[i].getRandomCustomers();
+    branchName[i].coockiesSoldByHour();
+    branchName[i].render();
+}
 
 
 
 
-        // this function generate a number randolmy between maxcust and mincust
-        function generateRandomNumbers(maxCust, minCust) {
-            var randomValue = Math.random();
-            return Math.floor(randomValue * (maxCust - minCust + 1) + minCust);
 
-        };
 
+
+function tableMaker() {
+    table.innerHTML ="";
+    headerMaker();
+    for( var i =0; i <branchName.length;i++){
+        branchName[i].render();
+    }
+    footerMaker();
+    
+}
+
+
+
+
+
+
+
+// this function generate a number randolmy between maxcust and mincust
+function generateRandomNumbers(maxCust, minCust) {
+    var randomValue = Math.random();
+    return Math.floor(randomValue * (maxCust - minCust + 1) + minCust);
+
+};
+
+
+footerMaker();
+
+function alerting() {
+    event.preventDefault();
+
+
+    var standLocationValue = event.target.s.value;
+    console.log(standLocationValue);
+    var minCustValue = parseInt(event.target.mincust.value);
+    console.log(minCustValue);
+    var maxCustValue = parseInt(event.target.maxcust.value);
+    console.log(maxCustValue);
+    var avagrgeSoldValue = parseFloat(event.target.avarageSold.value);
+    console.log(avagrgeSoldValue)
+
+
+    var newStandLocation = new Branch(standLocationValue, minCustValue, maxCustValue, avagrgeSoldValue)
+
+    newStandLocation.getRandomCustomers();
+    newStandLocation.coockiesSoldByHour();
+    // var rowCount = table.rows.length
+    // table.deleteRow[rowCount]
+    
+    
+    
+   tableMaker();
+    console.log(branchName)
+
+    // table.removeChild(tr1);
+    //  newStandLocation.render(table);
+    
+    console.log("newStandLocation");
+
+}
+var newStand = document.getElementById('new-stand');
+newStand.addEventListener("submit", alerting);
